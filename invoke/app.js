97cbd8422091a75bfaec6f52c27ae5c4ebec3e12
MussaKhan74 <MarkeLoof@gmail.com>
2025-08-20 08:28:48 +0500

    add common llms settings

--- invoke/app.js
+++ invoke/app.js
@@ -24,8 +24,8 @@ async function main() {
         messages: [
             {
                 role: "system",
-                content:
-                    "You are Jarvis, a smart review grader. Your task is to analyse given review and return the sentiment. Clasify the review as Positive, Negative or Neutral. Output must be a single word.",
+                content: `You are Jarvis, a smart review grader. Your task is to analyse given review and return the sentiment. Clasify the review as Positive, Negative or Neutral. You must return the result in valid JSON structure.
+                example: { "sentiment": "string" }`,
             },
             {
                 role: "user",
