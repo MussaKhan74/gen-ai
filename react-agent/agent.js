3973a3362147d6892abb0a276d64c28bf7590cfc
MussaKhan74 <MarkeLoof@gmail.com>
2025-08-31 08:23:14 +0000

    add short term memory for react agent

--- react-agent/agent.js
+++ react-agent/agent.js
@@ -1,7 +1,9 @@
+import readline from "node:readline/promises";
 import { tool } from "@langchain/core/tools";
 import { ChatGroq } from "@langchain/groq";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { TavilySearch } from "@langchain/tavily";
+import { MemorySaver } from "@langchain/langgraph";
 import z from "zod";
 
 async function main() {
@@ -33,28 +35,45 @@ async function main() {
     }
   );
 
+  const checkpointer = new MemorySaver();
+
   const agent = createReactAgent({
     llm: model,
     tools: [search, calendarEvents],
+    checkpointer: checkpointer,
   });
 
-  const result = await agent.invoke({
-    messages: [
-      {
-        role: "system",
-        content: `You are a personal assistant. Use provided tools to get the information if you don't have it. Current date and time: ${new Date().toUTCString()}`,
-      },
-      {
-        role: "user",
-        content: "Hi, Do I have any meeting today?",
-      },
-    ],
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  console.log(
-    "Assistant: ",
-    result.messages[result.messages.length - 1].content
-  );
+  while (true) {
+    const userQuery = await rl.question("You: ");
+
+    if (userQuery === "bye") break;
+
+    const result = await agent.invoke(
+      {
+        messages: [
+          {
+            role: "system",
+            content: `You are a personal assistant. Use provided tools to get the information if you don't have it. Current date and time: ${new Date().toUTCString()}`,
+          },
+          {
+            role: "user",
+            content: userQuery,
+          },
+        ],
+      },
+      { configurable: { thread_id: "1" } }
+    );
+
+    console.log(
+      "Assistant: ",
+      result.messages[result.messages.length - 1].content
+    );
+  }
 }
 
 main();
