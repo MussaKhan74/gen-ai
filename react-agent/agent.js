d5ce34064c6727c026bf77d6ca87a8cd959ca8bc
MussaKhan74 <MarkeLoof@gmail.com>
2025-08-31 06:40:31 +0000

    add tool calling in react agent

--- react-agent/agent.js
+++ react-agent/agent.js
@@ -1,6 +1,8 @@
+import { tool } from "@langchain/core/tools";
 import { ChatGroq } from "@langchain/groq";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { TavilySearch } from "@langchain/tavily";
+import z from "zod";
 
 async function main() {
   const model = new ChatGroq({
@@ -13,16 +15,38 @@ async function main() {
     topic: "general",
   });
 
+  const calendarEvents = tool(
+    async ({ query }) => {
+      // Google calendar logic goes here...
+      return JSON.stringify([
+        { title: "Meeting with Essa", time: "2 PM", location: "Gmeet" },
+      ]);
+    },
+    {
+      name: "get-calender-events",
+      description: "Call to get the calendar events",
+      schema: z.object({
+        query: z
+          .string()
+          .describe("The query to use in your calendar event search."),
+      }),
+    }
+  );
+
   const agent = createReactAgent({
     llm: model,
-    tools: [search],
+    tools: [search, calendarEvents],
   });
 
   const result = await agent.invoke({
     messages: [
       {
+        role: "system",
+        content: `You are a personal assistant. Use provided tools to get the information if you don't have it. Current date and time: ${new Date().toUTCString()}`,
+      },
+      {
         role: "user",
-        content: "What is current weather in Multan today ?",
+        content: "Hi, Do I have any meeting today?",
       },
     ],
   });
